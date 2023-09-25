40e7271928461792a128a4b2b78d31959a8de7d8
DESKTOP-FAD57M7\daw1 <dawdmelendezm@iescastelar.com>
2023-09-25 16:41:08 +0200

    Nueva linea de codigo

--- HolaMundo/src/App.java
+++ HolaMundo/src/App.java
@@ -3,5 +3,6 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Mi nombre es Daniel");
         System.out.println("Mi apellido es Melendez");
+        System.out.println("Mi edad es 25 años");
     }
 }
